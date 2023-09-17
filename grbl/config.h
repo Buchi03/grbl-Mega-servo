142c5a0bd1afe022ed7b4e0f025cbaabf0467835
Buchegger Thomas <thomas.buchegger@s7-rail.com>
2023-09-17 22:25:54 +0200

    feat: change y endstop from min to max

--- grbl/config.h
+++ grbl/config.h
@@ -202,9 +202,10 @@
 #ifdef DEFAULTS_RAMPS_BOARD
   // Only enable the following line if you have - (min) limit switches attached
   //#define INVERT_MIN_LIMIT_PIN_MASK ((1<<X_AXIS) | (1<<Y_AXIS) | (1<<Z_AXIS))
-  #define INVERT_MIN_LIMIT_PIN_MASK ((1<<X_AXIS) | (1<<Y_AXIS))
+  #define INVERT_MIN_LIMIT_PIN_MASK (1<<X_AXIS)
   // Only enable the following line if you have + (max) limit switches attached
   //#define INVERT_MAX_LIMIT_PIN_MASK ((1<<X_AXIS) | (1<<Y_AXIS) | (1<<Z_AXIS))  
+  #define INVERT_MAX_LIMIT_PIN_MASK (1<<Y_AXIS)
 #endif
 
 // Inverts the spindle enable pin from low-disabled/high-enabled to low-enabled/high-disabled. Useful
